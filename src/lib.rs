a67b95d84648e7409c1afdfb8060aa8d2d6cd7af
Cédric Meuter <cedric.meuter@gmail.com>
2023-09-17 19:44:17 +0200

    doc: added documentation for the Archive type

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 mod format;
 
 #[allow(deprecated)]
-pub use format::{Format, ArchiveKind};
+pub use format::{ArchiveKind, Format};
 
 use std::{fs::File, io::ErrorKind, path::Path};
 
@@ -41,9 +41,36 @@ impl<R: ::std::io::Read> Archived for tar::Archive<R> {
     }
 }
 
+/// A collection of files, possibly compressed (e.g. `tgz`, `zip`, ...).
+///
+/// # Supported Formats
+///
+/// - `sample.zip` (requires the zip feature).
+/// - `sample.tar` (requires the `tar` feature).
+/// - `sample.tgz` or `sample.tar.gz` (requires `tar` and `gzip` features).
+/// - `sample.tar.xz` (requires `tar` and `xz` features).
+/// - `sample.tar.bz2` (requires `tar` and `bzip` features).
+/// - `sample.tar.zstd` or `sample.tar.zst` (requires `tar` and `zstd` features).
 pub struct Archive(Box<dyn Archived>);
 
 impl Archive {
+    /// Opens an archive stored on the filesystem.
+    ///
+    /// The format of the archive will be inferred from the file
+    /// extension. See [infer_from_file_extension](Format::infer_from_file_extension).
+    ///
+    /// # Arguments:
+    ///
+    /// - `path`: the path to the archive file to open
+    ///
+    /// # Examples:
+    ///
+    /// ```
+    /// use arkiv::Archive;
+    ///
+    /// let archive = Archive::open("path/to/archive.zip");
+    /// ```
+    ///
     pub fn open(path: impl AsRef<Path>) -> Result<Self> {
         let _file = File::open(&path)?;
         match Format::infer_from_file_extension(path) {
@@ -77,13 +104,54 @@ impl Archive {
         }
     }
 
+    /// Returns the list of entries stored within the archive.
+    ///
+    /// # Warning
+    ///
+    /// For convenience, these entries are returned as an already
+    /// collected `Vec<String>`. If the archive contains a large
+    /// number of files, the amount of memory required to store
+    /// these entries might be large.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use arkiv::{Archive, Result};
+    ///
+    /// fn main() -> Result<()> {
+    ///    let mut archive = Archive::open("path/to/archive.tgz")?;
+    ///    let entries: Vec<String> = archive.entries()?;
+    ///
+    ///    for entry in entries {
+    ///        print!("{entry}");
+    ///    }
+    ///    Ok(())
+    /// }
+    /// ```
+    ///
+    ///
     pub fn entries(&mut self) -> Result<Vec<String>> {
         self.0.entries()
     }
 
+    /// unpacks the contents of the archive.
+    ///
+    /// # Arguments
+    ///
+    /// - `dest`: the destination folder (will be created if necessary)
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use arkiv::{Archive, Result};
+    //
+    /// fn main() -> Result<()> {
+    ///    let mut archive = Archive::open("path/to/archive.tgz")?;
+    ///    archive.unpack("/tmp/extracted/")?;
+    ///    Ok(())
+    /// }
+    /// ```
     pub fn unpack(&mut self, dest: impl AsRef<Path>) -> Result<()> {
         self.0.unpack(dest.as_ref())
     }
 }
-
-
