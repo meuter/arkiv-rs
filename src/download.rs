4d0078ca15441b2342afee8901ee608ae353534d
Cédric Meuter <cedric.meuter@gmail.com>
2023-09-30 13:41:20 +0200

    feat: added `Downloader::on_progress`
    One can now register a callback that will be called during the
    download of the archive with some progress imformation.

--- src/download.rs
+++ src/download.rs
@@ -3,7 +3,10 @@ use ureq::Response;
 use crate::{archive::Storage, Archive};
 
 use super::{Error, Result};
-use std::path::{Path, PathBuf};
+use std::{
+    io::{ErrorKind, Write},
+    path::{Path, PathBuf},
+};
 
 /// URL is missing in [Downloader].
 pub struct UrlMissing;
@@ -22,6 +25,12 @@ pub enum DestProvided {
     /// Archive file will be downloaded in a specific directory.
     Dir(PathBuf),
 }
+
+/// Progress callback is not provided in [Downloader]
+pub struct OnProgressNotProvided;
+
+/// Progress callback is provided in [Downloader]
+pub struct OnProgressProvided<F: FnMut(u64, u64)>(F);
 
 /// Allows to download an archive file and open it. This struct
 /// provides a bit more flexibility compared to [Archive::download]
@@ -44,48 +53,60 @@ pub enum DestProvided {
 /// }
 ///
 /// ```
-pub struct Downloader<U, D> {
+pub struct Downloader<U, D, O> {
     url: U,
     dest: D,
+    on_progress: O,
 }
 
-impl Downloader<UrlMissing, DestMissing> {
+impl Downloader<UrlMissing, DestMissing, OnProgressNotProvided> {
     /// Returns a new `Downloader`.
     pub fn new() -> Self {
         Self::default()
     }
 }
 
-impl Default for Downloader<UrlMissing, DestMissing> {
+impl Default for Downloader<UrlMissing, DestMissing, OnProgressNotProvided> {
     /// Returns a default `Downloader`.
     fn default() -> Self {
         Self {
             url: UrlMissing,
             dest: DestMissing,
+            on_progress: OnProgressNotProvided,
         }
     }
 }
 
-impl<D> Downloader<UrlMissing, D> {
+impl<D, O> Downloader<UrlMissing, D, O> {
     /// Allows to specify the URL to the archive that should be downloaded.
     ///
     /// # Arguments
     /// - `url`: the URL to the archive to downaload
     ///
-    pub fn url(self, url: impl AsRef<str>) -> Downloader<UrlProvided, D> {
+    pub fn url(self, url: impl AsRef<str>) -> Downloader<UrlProvided, D, O> {
         let url = UrlProvided(url.as_ref().to_string());
         let dest = self.dest;
-        Downloader { url, dest }
+        let on_progress = self.on_progress;
+        Downloader {
+            url,
+            dest,
+            on_progress,
+        }
     }
 }
 
-impl<U> Downloader<U, DestMissing> {
+impl<U, O> Downloader<U, DestMissing, O> {
     /// Allows to specify that the downloaded archive file should be stored
     /// in the temporary directory.
-    pub fn to_temp(self) -> Downloader<U, DestProvided> {
+    pub fn to_temp(self) -> Downloader<U, DestProvided, O> {
         let url = self.url;
         let dest = DestProvided::TempDir;
-        Downloader { url, dest }
+        let on_progress = self.on_progress;
+        Downloader {
+            url,
+            dest,
+            on_progress,
+        }
     }
 
     /// Allows to specify that the downloaded archive file should be stored
@@ -97,14 +118,47 @@ impl<U> Downloader<U, DestMissing> {
     /// # Arguments
     /// - `dest`: the destination directory.
     ///
-    pub fn to_directory(self, dest: impl AsRef<Path>) -> Downloader<U, DestProvided> {
+    pub fn to_directory(self, dest: impl AsRef<Path>) -> Downloader<U, DestProvided, O> {
         let url = self.url;
         let dest = DestProvided::Dir(dest.as_ref().to_path_buf());
-        Downloader { url, dest }
+        let on_progress = self.on_progress;
+        Downloader {
+            url,
+            dest,
+            on_progress,
+        }
     }
 }
 
-impl Downloader<UrlProvided, DestProvided> {
+impl<U, D> Downloader<U, D, OnProgressNotProvided> {
+    /// Sets a callback that will be regularily called during the download to
+    /// nonitor the progress.
+    ///
+    /// # Arguments
+    ///
+    /// - `callback`: closure that will be called with two values:
+    ///     - the current number of bytes already downloaded
+    ///     - the total number of bytes that needs to be downloaded
+    ///
+    /// # Example
+    ///
+    ///
+    pub fn on_progress<F>(self, callback: F) -> Downloader<U, D, OnProgressProvided<F>>
+    where
+        F: FnMut(u64, u64),
+    {
+        let url = self.url;
+        let dest = self.dest;
+        let on_progress = OnProgressProvided(callback);
+        Downloader {
+            url,
+            dest,
+            on_progress,
+        }
+    }
+}
+
+impl<O> Downloader<UrlProvided, DestProvided, O> {
     fn storage(&self) -> Result<Storage> {
         let file_name = Path::new(&self.url.0)
             .file_name()
@@ -123,7 +177,7 @@ impl Downloader<UrlProvided, DestProvided> {
     }
 }
 
-impl<D> Downloader<UrlProvided, D> {
+impl<D, O> Downloader<UrlProvided, D, O> {
     fn get(&self) -> Result<Response> {
         let response = ureq::get(&self.url.0)
             .call()
@@ -132,7 +186,7 @@ impl<D> Downloader<UrlProvided, D> {
     }
 }
 
-impl Downloader<UrlProvided, DestProvided> {
+impl Downloader<UrlProvided, DestProvided, OnProgressNotProvided> {
     /// Downloads the archive and opens it. Return an [Archive]. If the
     /// the archive file was downloaded to a temporary directory, the file will
     /// be deleted once the [Archive] is dropped.
@@ -146,5 +200,44 @@ impl Downloader<UrlProvided, DestProvided> {
         std::io::copy(&mut source, &mut dest)?;
 
         Archive::new(storage)
+    }
+}
+
+impl<F: FnMut(u64, u64)> Downloader<UrlProvided, DestProvided, OnProgressProvided<F>> {
+    /// Downloads the archive and opens it. Return an [Archive]. If the
+    /// the archive file was downloaded to a temporary directory, the file will
+    /// be deleted once the [Archive] is dropped.
+    ///
+    /// During the download the provided progress callback will be called.
+    pub fn download(mut self) -> Result<Archive> {
+        let response = self.get()?;
+        let content_length = response
+            .header("content-length")
+            .ok_or(Error::InvalidRequest(
+                "response does not contain 'content-length' header".to_string(),
+            ))?
+            .parse::<u64>()
+            .map_err(|err| {
+                Error::InvalidRequest(format!(
+                    "'content-length' in the response header could not be parsed '{err}'"
+                ))
+            })?;
+        let storage = self.storage()?;
+        let mut source = response.into_reader();
+        let mut dest = storage.create()?;
+
+        let mut buf = [0; 16384];
+        let mut written = 0;
+        loop {
+            self.on_progress.0(written as u64, content_length);
+            let bytes_read = match source.read(&mut buf) {
+                Ok(0) => return Archive::new(storage),
+                Ok(len) => len,
+                Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+                Err(e) => return Err(e.into()),
+            };
+            dest.write_all(&buf[..bytes_read])?;
+            written += bytes_read;
+        }
     }
 }
--- tests/download.rs
+++ tests/download.rs
@@ -1,10 +1,10 @@
 #[cfg(feature = "download")]
 mod download {
 
-    use arkiv::{Archive, Error as ArkivError};
+    use arkiv::{Archive, Downloader, Error as ArkivError};
     use httptest::{matchers::request, responders::status_code, Expectation, Server};
     use std::{
-        fs::File,
+        fs::{self, File},
         io::{BufReader, Read},
         path::Path,
     };
@@ -36,6 +36,51 @@ mod download {
 
         let res = Archive::download(url.to_string());
         assert!(matches!(res, Err(ArkivError::InvalidRequest(_))));
+
+        Ok(())
+    }
+
+    #[allow(unused)]
+    async fn test_progress(path: impl AsRef<Path>) -> Result<()> {
+        // read archive contents into buffer
+        let archive_file = File::open(path.as_ref())?;
+        let mut reader = BufReader::new(archive_file);
+        let mut buffer = Vec::new();
+        reader.read_to_end(&mut buffer)?;
+
+        // prepare test server to return archive contents on request
+        let server = Server::run();
+        server.expect(
+            Expectation::matching(request::method_path(
+                "GET",
+                format!("/{}", path.as_ref().display()),
+            ))
+            .respond_with(status_code(200).body(buffer)),
+        );
+
+        // download archive
+        let url = format!("/{}", path.as_ref().display());
+        let url = server.url(&url);
+
+        let mut callbacks: Vec<(u64, u64)> = vec![];
+        let mut archive = Downloader::new()
+            .url(url.to_string())
+            .to_temp()
+            .on_progress(|current, total| callbacks.push((current, total)))
+            .download()?;
+
+        // check callback was called at least at startup and finish
+        let total_filesize = fs::metadata(&path).unwrap().len();
+        assert!(!callbacks.is_empty());
+        assert!(callbacks.first().unwrap() == &(0, total_filesize));
+        assert!(callbacks.last().unwrap() == &(total_filesize, total_filesize));
+
+        // check the contents
+        let mut actual = archive.entries()?;
+        let mut expected = vec!["sample/", "sample/sample.txt"];
+        actual.sort();
+        expected.sort();
+        assert_eq!(actual, expected);
 
         Ok(())
     }
@@ -76,6 +121,12 @@ mod download {
     #[tokio::test]
     async fn download_404() -> Result<()> {
         test_404("tests/sample/sample.zip").await
+    }
+
+    #[tokio::test]
+    #[cfg(feature = "zip")]
+    async fn download_progress() -> Result<()> {
+        test_progress("tests/sample/sample.zip").await
     }
 
     #[tokio::test]
